d169d3d04e953cebe0ce9180c7ae0b2d3bff53fb
Edwin <win.fr8@gmail.com>
2021-07-29 11:12:35 +0700

    Add handlers on Bot online, joined a guild, and kicked from a guild
    - Register Guild Commands on bot online and joined a guild
    - Send welcome message on bot joined a guild
    
    Signed-off-by: Edwin <win.fr8@gmail.com>

--- service/delivery/bot/command/handler.go
+++ service/delivery/bot/command/handler.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -11,6 +12,7 @@ import (
 type botCommandHandler struct {
 	session *discordgo.Session
 	euc     service.EventUsecase
+	guilds  map[string]*discordgo.Guild
 }
 
 func NewBotCommandHandler(
@@ -20,6 +22,7 @@ func NewBotCommandHandler(
 	return &botCommandHandler{
 		session: session,
 		euc:     euc,
+		guilds:  map[string]*discordgo.Guild{},
 	}
 }
 
@@ -36,15 +39,54 @@ func (h *botCommandHandler) RegisterBotCommandHandlers() (err error) {
 		}
 	})
 
-	for _, v := range commands {
-		cmd, err := h.session.ApplicationCommandCreate(os.Getenv("BOT_APPLICATION_ID"), os.Getenv("GUILD_ID"), v)
-		if err != nil {
-			log.Printf("Cannot create '%v' command: %v\n", v.Name, err)
-			return err
+	// NOTE: Handler on Bot Online
+	h.session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		for _, g := range r.Guilds {
+			h.guilds[g.ID] = g
+
+			for _, v := range commands {
+				cmd, err := h.session.ApplicationCommandCreate(os.Getenv("BOT_APPLICATION_ID"), g.ID, v)
+				if err != nil {
+					log.Printf("Cannot create '%v' command on Guild [%s]: %v\n", v.Name, g.ID, err)
+					return
+				}
+
+				log.Printf("Command %s successfully registered on Guild [%s], ID:%s\n", cmd.Name, g.ID, cmd.ID)
+			}
+		}
+	})
+
+	// NOTE: Handler on Bot joined a Guild
+	h.session.AddHandler(func(s *discordgo.Session, g *discordgo.GuildCreate) {
+		if _, ok := h.guilds[g.ID]; ok {
+			return
 		}
 
-		log.Printf("Command %s successfully registered, ID:%s\n", cmd.Name, cmd.ID)
-	}
+		fmt.Printf("Me joined Guild #%s :D\n", g.ID)
+
+		for _, v := range commands {
+			cmd, err := h.session.ApplicationCommandCreate(os.Getenv("BOT_APPLICATION_ID"), g.ID, v)
+			if err != nil {
+				log.Printf("Cannot create '%v' command on Guild [%s]: %v\n", v.Name, g.ID, err)
+				return
+			}
+
+			log.Printf("Command %s successfully registered on Guild [%s], ID:%s\n", cmd.Name, g.ID, cmd.ID)
+		}
+
+		s.ChannelMessageSend(g.SystemChannelID, "Hello World!")
+	})
+
+	// NOTE: Handler on Bot kicked from a Guild
+	h.session.AddHandler(func(s *discordgo.Session, g *discordgo.GuildDelete) {
+		if _, ok := h.guilds[g.ID]; !ok {
+			return
+		}
+
+		fmt.Printf("Me is kicked from Guild #%s :(\n", g.ID)
+
+		delete(h.guilds, g.ID)
+	})
 
 	// NOTE: Add here for Other Command Handlers
 
