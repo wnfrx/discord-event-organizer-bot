56072c883ed123f1ec447cce0fcfb254d93fd8a3
Edwin <win.fr8@gmail.com>
2021-08-01 19:12:41 +0700

    Create database migrations
    Signed-off-by: Edwin <win.fr8@gmail.com>

---
+++ config/migration.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"errors"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func (c *Config) InitMigration() (err error) {
+	if c.db == nil {
+		return errors.New("database connection is not initialized yet")
+	}
+
+	driver, err := postgres.WithInstance(c.db.DB, &postgres.Config{})
+	if err != nil {
+		log.Fatalf("Failed to init migration postgres instance: %v", err)
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///db/migrations",
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		log.Fatalf("Failed to init migration with database instance: %v", err)
+		return err
+	}
+
+	if err = m.Up(); err != nil {
+		log.Fatalf("Failed to do migration: %v", err)
+		return err
+	}
+
+	return nil
+}
